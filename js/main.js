a72a758d14ac4524ed3fb5ac0bfc1b738af98e40
Седона <enotov.92@list.ru>
2020-06-08 12:09:00 +0300

    ÐÐÑСделал первое задание

---
+++ js/main.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var PIN_WIDTH = 40;
+var PIN_HEIGHT = 40;
+
+var OFFERS_COUNT = 8;
+
+var  OFFERS_TITLES = [
+  'Большая квартира',
+  'Маленькая квартира',
+  'Ограмный красивый дворец',
+  'Маленький дворец',
+  'Красивый домик',
+  'Некрасивый домик',
+  'Уютное бунгало',
+  'Неуютное бунгало',
+];
+
+var TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+
+var CHECK_TIME = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var  FEATURES_ARRAY = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+var PHOTOS_ARRAY = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+// Для генерации случайных значений в диапозоне
+var getRandomFromTo = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Для значений в случайном порядке которые неповторяются
+
+var compareRandom = function () {
+  return Math.random() - 0.5;
+};
+
+var getAvatar = function (num) {
+  return 'img/avarars/users0' + (num + 1) + '.png';
+};
+
+var getPhotos = function () {
+  return PHOTOS_ARRAY.sort(compareRandom);
+};
+
+var getFeatures = function () {
+  FEATURES_ARRAY.sort(compareRandom);
+  return FEATURES_ARRAY.slice(0, getRandomFromTo(1, FEATURES_ARRAY.length));
+};
+
+var generateOffers = function () {
+  var offers = [];
+  OFFERS_TITLES.sort(compareRandom); // Для заголовков в рандомном порядке
+  for (var i = 0; i < OFFERS_COUNT; i++) {
+    var locX = 600;
+    var locY = 350;
+    offers.push({
+      author: {
+        avatar: getAvatar(i)
+      },
+      offer: {
+        title: OFFER_TITLES[i],
+        address: locX + ', ' + locY,
+        price: getRandomFromTo(1000, 1000000),
+        type: TYPES[getRandomFromTo(0, 2)],
+        rooms: getRandomFromTo(1, 5),
+        guests: getRandomFromTo(1, 10),
+        checkin: CHECK_TIME[getRandomFromTo(0, 2)],
+        checkout: CHECK_TIME[getRandomFromTo(0, 2)],
+        features: getFeatures(),
+        description: '',
+        photos: getPhotos()
+      },
+      location: {
+        x: locX,
+        y: locY
+      }
+    });
+  }
+  return offers;
+};
+
+var showMap = function () {
+  var mapBlock = document.querySelector('.map');
+  mapBlock.classList.remove('map-faded');
+};
+
+var renderPins = function (renderingOffer) {
+  var pinTemplate = document.querySelector('template').content;
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.querySelector('.popup__avatar').src = renderingOffer.author.avatar;
+};
+
+var pinIcon = pinElement.querySelector('.map__pin');
+pinIcon.querySelector('img').src = renderingOffer.author.avatar;
+pinIcon.style.left = (renderingOffer.location.x + PIN_WIDTH / 2) + 'px';
+pinIcon.style.top = (renderingOffer.location.y + PIN_HEIGHT) + 'px';
+return pinElement;
+};
+
+var setupPins = function () {
+  var offersArray = generateOffers();
+  var fragment = document.createDocumentFragment();
+  var similarListElement = document.querySelector('.map__pins');
+  for (var n = 0; n < offersArray.length; n++) {
+    fragment.appendChild(renderPins(offersArray[n]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+showMap();
+setupPins();
